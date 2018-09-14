f3c8b8a6383062b02e49bfb63b8e39f4111e5410
wildpow <element1028@gmail.com>
2018-09-13 22:54:17 -0700

    added back gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,19 @@ module.exports = {
         endpoint: `https://api.graphcms.com/simple/v1/cjlrp4qcs70qt0178mvh6gfn5`,
         query: require("./gatsby/configQuery")
       }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+      name: `E.S.C Mattress Center`,
+      short_name: `E.S.C`,
+      start_url: `/`,
+      background_color: `#ffffff`,
+      theme_color: `#1565c0`,
+      display: `minimal-ui`,
+      icon: `src/images/logo.png`, // This path is relative to the root of the site.
+    },
+    },
   
   ],
 }
