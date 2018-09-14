fdd200ab48364c60a0fd58b7bff3bb3e582e0f36
wildpow <element1028@gmail.com>
2018-09-13 22:10:39 -0700

    added 2 forms

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import MakeOffer from '../components/makeOffer';
 
 const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <MakeOffer/>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Contact from '../components/contact';
 
 const SecondPage = () => (
   <div>
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+    <Contact/>
     <Link to="/">Go back to the homepage</Link>
   </div>
 )
